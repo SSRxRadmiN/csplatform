6cab1ec6266605a030b6ba78edbe09259e993d33
Radmin <allofnfs@gmail.com>
2026-03-14 14:55:40 +0200

    rename
    rename

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= esc($title ?? 'CS Headshot') ?></title>
+    <title><?= esc($title ?? 'CS Headshot | РЕАЛЬНІ КАБАНИ Public Server CS 1.6') ?></title>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/app.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/favicon/site.webmanifest">
 </head>
 <body>
 
